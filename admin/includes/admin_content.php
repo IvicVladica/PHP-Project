98463159fa42d883d2a6e38832d57649ed62bc14
IvicVladica <ivicvladica@gmail.com>
2023-06-05 20:54:18 +0200

    User class created

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,16 +8,38 @@
             <small>Subheading</small>
         </h1>
 
+
+
         <?php
         
-        $sql = "SELECT * FROM users WHERE id=1";
-        $result = $database->query($sql);
-        $user_found = mysqli_fetch_array($result);
+    //    $result_set = User::find_all_users();
 
-        echo $user_found['username'];
+    //    while($row = mysqli_fetch_array($result_set)) {
+
+    //     echo $row['username'] . "<br>";
+
+    //     }
+
+    //    $found_user = User::find_user_by_id(2);
+
+    //    $user = User::instantiation($found_user);
+       
+    //     echo $user->id;
+
+        $users = User::find_all_users();
+
+        foreach($users as $user) {
+            echo $user->username . "<br>";
+        }
+
         
+        $found_user = User::find_user_by_id(2);
+        echo $found_user->username;
+
+
 
         ?>
+
 
 
         <ol class="breadcrumb">
