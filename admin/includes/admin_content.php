8099ddab0a1c8153085f9983453d428b137f41f0
IvicVladica <ivicvladica@gmail.com>
2023-06-09 13:06:38 +0200

    Photo class created

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -33,21 +33,40 @@
 
 
         // $user = User::find_user_by_id(16);
-        // $user->delete();
+        // $user->delete();         //proba Delete
 
 
 
-        // $user = User::find_user_by_id(16);
+        // $user = User::find_by_id(19);
         // $user->username = "username1";
         // $user->password = "password1";
         // $user->first_name = "fn1";
         // $user->last_name = "ln1";
-        // $user->save();
+        // $user->save();               //proba Update
 
         // $user = new User();
         // $user->username = "SaveProbaNewUser2";
         // $user->password = "SaveProbaNewPassword2";
-        // $user->save();
+        // $user->save();           //proba Create
+
+        // $users = User::find_all();
+
+        // foreach ($users as $user) {
+        //     echo $user->username."</br>";     //proba nakon uvodjenja db-object klase
+        // }
+
+        // $photos = Photo::find_all();
+        // foreach ($photos as $photo) {
+        //     echo $photo->title."</br>";     //proba nakon uvodjenja db-object klase
+        // }
+
+        // $photo = new Photo();
+        // $photo->title = "Slika2";
+        // $photo->description = "Opis slike2";
+        // $photo->save();           //proba Create
+   
+            // echo SITE_ROOT . "</br>";
+            // echo INCLUDES_PATH;
 
         ?>
 
