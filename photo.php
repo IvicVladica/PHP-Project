a5c780cb2c7d9b6964100625b25de63dafccca89
IvicVladica <ivicvladica@gmail.com>
2023-06-13 11:31:31 +0200

    Admin comments started - comment class and methods

--- photo.php
+++ photo.php
@@ -1,3 +1,45 @@
+<?php
+
+require_once("admin/includes/init.php");
+
+if(empty($_GET['id'])) {
+    redirect("index.php");
+}
+
+$photo = Photo::find_by_id($_GET['id']);
+
+
+if(isset($_POST['submit'])) {
+
+    $author = trim($_POST['author']);
+    $abody = trim($_POST['body']);
+
+    $new_comment = Comment::create_comment($photo->id, $author, $body);
+
+    if($new_comment && $new_comment->save()) {
+
+        redirect("photo.php?id={$photo->id}");
+
+    } else {
+
+        $message = "There was some problems saving comment";
+
+    }
+    
+} else {
+
+    $author = "";
+    $body = "";
+
+}
+
+Comment::find_the_comments($photo->id);
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -102,20 +144,33 @@
                 <!-- Blog Comments -->
 
                 <!-- Comments Form -->
+
+
+
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form role="form" method="post">
                         <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                            <label for="author">Author</label>
+                            <input type="text" name="author" class="form-control" >
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                            <textarea name="body" class="form-control" rows="3"></textarea>
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
+
+
+
 
                 <hr>
 
                 <!-- Posted Comments -->
 
+
+
+
                 <!-- Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
@@ -129,31 +184,6 @@
                     </div>
                 </div>
 
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </div>
-                        <!-- End Nested Comment -->
-                    </div>
-                </div>
 
             </div>
 
