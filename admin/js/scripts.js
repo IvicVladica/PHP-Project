2c60892ad9d72ba876191ae8360903a37d2b6e66
IvicVladica <ivicvladica@gmail.com>
2023-06-16 20:21:14 +0200

    Ajax created for setting of User Image

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     var user_id;
     var image_src;
     var image_href_splitted;
-    var image_id;
+    var image_name;
 
     $(".modal_thumbnails").click(function(){
 
@@ -21,14 +21,34 @@ $(document).ready(function() {
 
         image_src = $(this).prop("src");
         image_href_splitted = image_src.split("/");
-        image_id = image_href_splitted[image_href_splitted.length - 1];
-
-        alert(image_id);
+        image_name = image_href_splitted[image_href_splitted.length - 1];
 
     });
+
+    $("#set_user_image").click(function(){
+
+        $.ajax({
+
+            url: "includes/ajax_code.php",
+            data: {image_name: image_name, user_id: user_id},
+            type: "POST",
+            success: function(data) {
+
+                if(!data.error) {
+
+                $(".user_image_box a img").prop('src', data);
+
+                }
+
+            }
+
+        });
+
+    });
+
 
     tinymce.init({selector:'textarea'});
 
 });
 
-//http://localhost/gallery/admin/delete_user.php?id,21
+
